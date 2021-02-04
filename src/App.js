0684a6634549c769c31ca16482153c920f736794
Marc <marcllopis89@gmail.com>
2021-02-04 13:36:47 +0100

    Adds redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class App extends Component {
+
+//   state= {
+//     count: 0
+//   }
+
+//   increment = () => this.setState({count: this.state.count + 1})
+
+//   decrease = () => this.setState({count: this.state.count - 1})
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.count}</h1>
+//         <button onClick={this.decrease}>-</button>
+//         <button onClick={this.increment}>+</button>
+//       </div>
+//     );
+//   }
+// }
+
+
+
+import {connect} from 'react-redux';
+import {increaseCounter, decreaseCounter} from './actions';
+
+class App extends Component {
+
+  increment = () => this.props.dispatch(increaseCounter(this.props.count))
+
+  decrease = () => this.props.dispatch(decreaseCounter(this.props.count))
+
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        <h1>{this.props.hello}</h1>
+        <h1>{this.props.count}</h1>
+        <button onClick={this.decrease}>-</button>
+        <button onClick={this.increment}>+</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+function mapStateToProps(state){
+  return {
+    count: state.count,
+    hello: state.hello,
+  }
+}
+
+
+export default connect(mapStateToProps)(App);
---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+export const INCREASE = 'INCREASE';
+export const DECREASE = 'DECREASE';
+
+
+
+export function increaseCounter(count) {
+  return {
+    type: INCREASE,
+    count
+  }
+}
+
+export function decreaseCounter(count) {
+  return {
+    type: DECREASE,
+    count
+  }
+}
